65e3bf08ae885c7814f96faecb51156d2068de09
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-21 21:50:31 +0300

    started on navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import './Navbar.css';
-
+import "./Navbar.css";
+import images from "../../constants/images";
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo"></div>
+  </nav>
 );
 
 export default Navbar;
