032769220778e5288f73ffa80bce45a0cf2acf11
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-22 16:53:35 +0300

    static menu section completed

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = (props) => (
+  <div className="app__menuItem">
+    <div className="app__menuItem-head">
+      <div className="apap__menuItem-name">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {props.title}
+        </p>
+      </div>
+      <div className="app__menuItem-dash" />
+      <div className="app__menuItem-price">
+        <p className="p__cormorant">{props.price}</p>
+      </div>
+    </div>
+    <div className="app__menuItem-sub">
+      <p className="p__opensanas" style={{ color: "#AAA" }}></p>
+    </div>
   </div>
 );
 
