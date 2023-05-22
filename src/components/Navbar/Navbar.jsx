f645ebd4b6f1be90dc838a4858650e2482397ad2
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-22 10:43:29 +0300

    add the logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
-
 import "./Navbar.css";
 import images from "../../constants/images";
 const Navbar = () => (
   <nav className="app__navbar">
-    <div className="app__navbar-logo"></div>
+    <div className="app__navbar-logo">
+      <img src={images.maison} alt="logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#home">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#home">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#home">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#home">Contact</a>
+      </li>
+    </ul>
   </nav>
 );
 
