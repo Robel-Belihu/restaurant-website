43cbe2a738a278b0758a51ecf0c9797b5eff95b4
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-22 15:27:13 +0300

    static header completed

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex-center">
+      <img src={images.G} alt="letter g" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="spoon_image" className="spoon__img" />
+        <p className="p__opensans">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et.
+        </p>
+        <button type="button" className="custom__button">
+          Explore More
+        </button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="aboutus_knife" />
+      </div>
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="spoon_image" className="spoon__img" />
+        <p className="p__opensans">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et.
+        </p>
+        <button type="button" className="custom__button">
+          Explore More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,8 +9,18 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavor" />
       <h1 className="app__header-h1">Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        We pride ourselves in following the traditional Dining seasonings and
+        processes used all over the world. Here, ancient customs and future
+        trends coexist, and the intricacies of African culture blend seamlessly
+        together over an Ethiopian-brewed coffee, or a bowl of Senegalese
+        black-eyed pea stew and a side of spicy Ghanaian plantains.
+      </p>
+      <button className="custom__button">Explore Menu</button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header image" />
+    </div>
   </div>
 );
 
