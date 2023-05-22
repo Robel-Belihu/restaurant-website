e26402e968d4a503b7226af65cbb9c3c809b9d22
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-22 22:32:52 +0300

    media query for menu section completed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse ">
+      <img src={images.Chef} alt="Chef_img" />
+    </div>
   </div>
 );
 
