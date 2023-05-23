50f85af660b36e3a4df9b3c4ae5e62d717a9efc7
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-23 10:55:11 +0300

    Chef section competed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { images } from "../../constants";
 import { SubHeading } from "../../components";
-import "./Chef.css";
 
 const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse ">
-      <img src={images.Chef} alt="Chef_img" />
+      <img src={images.chef} alt="Chef_img" />
+    </div>
+    <div className="app__wrapper_info ">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis.
+          </p>
+        </div>
+        <p className="p__opensans">
+          praesentium voluptatum deleniti atque corrupti quos dolores et quas
+          molestias excepturi sint.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p style={{ color: "#fff" }}>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="signature" />
+      </div>
     </div>
   </div>
 );
