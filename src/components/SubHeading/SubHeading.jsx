276fe95f66f7e8a5458768a04e560386b3e952bb
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-24 22:43:27 +0300

    add gallery section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { images } from "../../constants";
 
-const SubHeading = ({ props }) => (
+const SubHeading = (props) => (
   <div style={{ marginBottom: "1rem" }}>
-    <p className="p__cormorant">Title</p>
+    <p className="p__cormorant">{props.title}</p>
     <img src={images.spoon} alt="spoon-img" className="spoon_img" />
   </div>
 );
